0856cc112a5296b214c377bb53af768cb20d084e
Jacob Trombetta <jacobtrombetta@gmail.com>
2021-03-31 15:40:46 -0400

    Loading single video frame in color.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <GLFW/glfw3.h>
 
+bool load_frame( const char* filename, int* width, int* height, unsigned char** data );
+
 int main( int argv, const char** argc){
     GLFWwindow* window;
 
@@ -8,36 +10,23 @@ int main( int argv, const char** argc){
         printf( "Couldn't init GLFW\n" );
         return 1;
     }
-    
-    window = glfwCreateWindow( 640, 480, "Hello world", nullptr, nullptr );
+
+    window = glfwCreateWindow( 640, 480, "Hello horse", nullptr, nullptr );
     if ( !window ){
         printf( "Couldn't init GLFW\n" );
         return 1;
     }
-    
-    unsigned char* data = new unsigned char[ 100 * 100 * 3 ];
-    for ( int row = 0; row < 100; row++ )
+
+    int frame_width, frame_height;
+    unsigned char* frame_data;
+    if ( !load_frame( "/Users/jacobtrombetta/Dropbox/documents/Development/video-playground/assets/production ID_4215784.mp4", &frame_width, &frame_height, &frame_data ) )
     {
-        for ( int col = 0; col < 100; col++ )
-        {
-            data[ row * 100 * 3 + col * 3     ] = 0xff;
-            data[ row * 100 * 3 + col * 3 + 1 ] = 0x00;
-            data[ row * 100 * 3 + col * 3 + 2 ] = 0x00;
-        }
+      printf( "Couldn't load video frame.\n" );
+      return 1;
     }
-    
-    for ( int row = 25; row < 75; row++ )
-    {
-        for ( int col = 25; col < 75; col++ )
-        {
-            data[ row * 100 * 3 + col * 3     ] = 0x00;
-            data[ row * 100 * 3 + col * 3 + 1 ] = 0x00;
-            data[ row * 100 * 3 + col * 3 + 2 ] = 0xff;
-        }
-    }
-    
+
     glfwMakeContextCurrent( window );
-    
+
     GLuint tex_handle;
     glGenTextures( 1, &tex_handle );
     glBindTexture( GL_TEXTURE_2D, tex_handle );
@@ -47,36 +36,36 @@ int main( int argv, const char** argc){
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
     glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
-    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, 100, 100, 0, GL_RGB, GL_UNSIGNED_BYTE, data );
-    
-    
+    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, frame_width, frame_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, frame_data );
+
+
     while ( !glfwWindowShouldClose( window ) ){
-        
+
         glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-        
+
         // Set up orphographic projection.
         int window_width, window_height;
         glfwGetFramebufferSize( window, &window_width, &window_height );
         glMatrixMode( GL_PROJECTION );
         glLoadIdentity();
-        glOrtho( 0, window_width, 0, window_height, -1, 1 );
+        glOrtho( 0, window_width, window_height, 0, -1, 1 );
         glMatrixMode( GL_MODELVIEW );
-        
+
         // Render whatever you want
         glEnable( GL_TEXTURE_2D );
         glBindTexture( GL_TEXTURE_2D, tex_handle );
         glBegin( GL_QUADS );
             glTexCoord2d( 0, 0 ); glVertex2i(   0,   0 );
-            glTexCoord2d( 1, 0 ); glVertex2i( 100,   0 );
-            glTexCoord2d( 1, 1 ); glVertex2i( 100, 100 );
-            glTexCoord2d( 0, 1 ); glVertex2i(   0, 100 );
+            glTexCoord2d( 1, 0 ); glVertex2i( frame_width,   0 );
+            glTexCoord2d( 1, 1 ); glVertex2i( frame_width, frame_height );
+            glTexCoord2d( 0, 1 ); glVertex2i(   0, frame_height );
         glEnd();
         glDisable( GL_TEXTURE_2D );
-        
+
         glfwSwapBuffers( window );
         glfwWaitEvents();
     }
-        
+
 
     return 0;
 }
